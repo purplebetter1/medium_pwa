46fe569eacdbda5fe3ac0f7098ac6045e901eaf0
Andrew <booleanorama@gmail.com>
2019-08-29 17:05:39 +0000

    new start_url

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'hello-pwa'
+var cacheName = 'hello-pwa';
 var filesToCache = [
   '/',
   'index.html',
@@ -11,7 +11,7 @@ var filesToCache = [
 self.addEventListener('install', function(e) {
 	e.waitUntil(
           caches.open(cacheName).then(function(cache){
-		return cache.addAll(filesToCache)
+		return cache.addAll(filesToCache);
    })
 
   );	
